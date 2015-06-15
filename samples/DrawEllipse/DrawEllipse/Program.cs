8f9e1ef3796af786693a5120810ee361b33b486d
J-M <jean-michel.perraud@csiro.au>
2015-06-16 09:43:31 +1000

    Needed to use dev.hold and dev.flush to get things working.

--- samples/DrawEllipse/DrawEllipse/Program.cs
+++ samples/DrawEllipse/DrawEllipse/Program.cs
@@ -20,7 +20,7 @@ namespace DrawEllipse
             // plot from R
             //to show outlier with method : classic & robust Mve 
 
-            e.Evaluate("n <- 100 ; spread <- (n/2 - abs(1:n - n/2))/n * 4");
+            e.Evaluate("n <- 100 ; spread <- (n/2 - abs(1:n - n/2))/n * (n/4)");
             e.Evaluate("X <- data.frame(1:n + spread * rnorm(n), 2 * 1:n + spread * rnorm(n))");
 
             int xAxis = 1;
@@ -31,19 +31,36 @@ namespace DrawEllipse
             e.Evaluate("center<- colMeans(X)");
             e.Evaluate("d2.95 <- qchisq(0.95, df = 2)");
             //e.Evaluate("gr<- grid(lty=3,col='lightgray', equilogs = 'TRUE')");
-            //dataform.rconn.Evaluate("mtext('with classical (red) and robust (blue)')");
             e.Evaluate("plot(x,y, main='Draw Ellipse ', pch=19,col='black', type='p')");
-            e.Evaluate("elp<- unname(ellipsoidPoints(shape, d2.95,center))");
-            Console.WriteLine("Pausing - press any key");
-            Console.ReadKey();
-            e.Evaluate(" lines(elp, col='red' , lty=7 , lwd=2)");
-            //e.Evaluate("lines(e)");
-            //e.Evaluate("lines(ellipsoidPoints(mve@cov, d2 = d2.95, loc=mve@center), col='blue', lty='7' , lwd='2') ");
-            // axGraphicsDevice2.RemoveFromConnector();
 
-            Console.WriteLine("Pausing - press any key");
+            Console.WriteLine("Plot - Pausing - press any key");
+            Console.ReadKey();
+
+            e.Evaluate("dev.hold()");
+            e.Evaluate("mtext('with classical (red) and robust (blue)')");
+            e.Evaluate("dev.flush()");
+            Console.WriteLine("Wrote mtext - Pausing - press any key");
+            Console.ReadKey();
+            e.Evaluate("elp<- unname(ellipsoidPoints(shape, d2.95,center))");
+            e.Evaluate("dev.hold()");
+            e.Evaluate(" lines(elp, col='red' , lty=7 , lwd=2)");
+            e.Evaluate("dev.flush()");
+            //e.Evaluate("lines(e)");
+            
+            Console.WriteLine("wrote lines - Pausing - press any key");
+            Console.ReadKey();
+
+            // What's mve???  Cannot test the following.
+            //e.Evaluate("lines(ellipsoidPoints(mve@cov, d2 = d2.95, loc=mve@center), col='blue', lty='7' , lwd='2') ");
+            //Console.WriteLine("Pausing - press any key");
+            // Console.ReadKey();
+
+
+            Console.WriteLine("Pausing - About to turn off character device - press any key");
             Console.ReadKey();
             e.Evaluate("dev.off()");
+            Console.WriteLine("Pausing - About to turn off R engine and exit - press any key");
+            Console.ReadKey();
             e.Dispose();
         }
     }
